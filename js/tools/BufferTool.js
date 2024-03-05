8345d43f9d01efc80135bf887f0e6e0b5abf42d4
Danny McVey <dannybmcvey@gmail.com>
2024-03-05 14:01:33 -0800

    remove random points logic from buffer tool

--- js/tools/BufferTool.js
+++ js/tools/BufferTool.js
@@ -30,32 +30,7 @@ export class BufferTool extends Tool {
      * Executes the RandomPointsTool logic, adding specified number of random points within a selected polygon.
      */
     execute() {
-        const polygonIdInput = document.getElementById('param-Input Layer');
-        const pointsCountInput = document.getElementById('param-Distance');
 
-        const polygonId = polygonIdInput ? parseInt(polygonIdInput.value, 10) : null;
-        const pointsCount = pointsCountInput ? parseInt(pointsCountInput.value, 10) : 0;
-
-        console.log(`Executing RandomPointsTool with Polygon ID: ${polygonId} and Points Count: ${pointsCount}`);
-        drawnItems.eachLayer(function(layer) {
-            // Check if the current layer matches the specified polygon ID
-            if (layer instanceof L.Polygon && layer._leaflet_id === polygonId) {
-                let polygon = layer.toGeoJSON();
-    
-                for (let i = 0; i < pointsCount; i++) {
-                    let pointAdded = false;
-                    while (!pointAdded) {
-                        let randomPoint = turf.randomPoint(1, {bbox: turf.bbox(polygon)}); // https://turfjs.org/docs/#randomPoint
-                        //todo: consider upgrade to turf.pointsWithinPolygon(points, searchWithin);
-                        if (turf.booleanPointInPolygon(randomPoint.features[0], polygon)) {
-                            let pointCoords = randomPoint.features[0].geometry.coordinates;
-                            L.marker([pointCoords[1], pointCoords[0]]).addTo(map);
-                            pointAdded = true;
-                        }
-                    }
-                }
-            }
-        });
     }
 
     // Dynamically populate dropdown when the tool is selected
