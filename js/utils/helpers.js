2eccbb3f0dd60c9ef27d8667010359bdc8b02dae
Danny McVey <dannybmcvey@gmail.com>
2024-03-18 21:52:04 -0700

    clean up

--- js/utils/helpers.js
+++ js/utils/helpers.js
@@ -1,31 +1,10 @@
 
-export function getCurrentVisibleExtent(map) {
-    console.log(map.getBounds())
-    var currentBounds = map.getBounds();
-    var southwest = [currentBounds._southWest.lat, currentBounds._southWest.lng];
-    var southeast = [currentBounds._southWest.lat, currentBounds._northEast.lng];
-    var northeast = [currentBounds._northEast.lat, currentBounds._northEast.lng];
-    var northwest = [currentBounds._northEast.lat, currentBounds._southWest.lng];
-    var visible_extent = turf.polygon([[southwest, southeast, northeast, northwest, southwest]]);
-    console.log(visible_extent);
-    return visible_extent;
-}
-
-
-// Log the current visible extent of the map to the console
+// Log the current visible extent of the map to the console and return the bounds in a format suitable for use with Turf.js
 export function logCurrentBounds(map) {
-    var bounds = map.getBounds(); // Get the current bounds
-    var southWest = bounds.getSouthWest(); // Get the South West corner
-    var northEast = bounds.getNorthEast(); // Get the North East corner
+    var bounds = map.getBounds(); 
+    var southWest = bounds.getSouthWest(); 
+    var northEast = bounds.getNorthEast(); 
 
-
-    // var southwest = [bounds._southWest.lat, bounds._southWest.lng];
-    // var southeast = [bounds._southWest.lat, bounds._northEast.lng];
-    // var northeast = [bounds._northEast.lat, bounds._northEast.lng];
-    // var northwest = [bounds._northEast.lat, bounds._southWest.lng];
-    // var visible_extent = turf.polygon([[southwest, southeast, northeast, northwest, southwest]]);
-    // console.log(visible_extent);
-    
     console.log("Current visible bounds:");
     console.log("South West corner: ", southWest);
     console.log("North East corner: ", northEast);
